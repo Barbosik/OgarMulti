2510b2b0a93db4090a193440ed702f8c81ab83fc
NatsuTheGreat <ry4n.berd@gmail.com>
2016-07-06 21:24:06 -0500

    Fix formatting #1

--- src/packet/ChatMessage.js
+++ src/packet/ChatMessage.js
@@ -11,14 +11,14 @@ function ChatMessage(gameServer, sender, message) {
 module.exports = ChatMessage;
 
 ChatMessage.prototype.build = function (protocol) {
-	// Variables
+    // Variables
     var text = this.message,
 	sender = this.sender,
 	config = this.gameServer.config,
 	color = { 'r': 0x9B, 'g': 0x9B, 'b': 0x9B },
 	name = "[SERVER]";
     
-	if (text == null) text = "";
+    if (text == null) text = "";
 	
     if (this.sender != null) {
         name = sender.getName();
@@ -26,12 +26,12 @@ ChatMessage.prototype.build = function (protocol) {
             if (sender.cells.length > 0) name = "An unnamed cell";
             else name = "Spectator";
         }
-		if (sender.socket.remoteAddress == config.serverAdminIP) {
-			color = { 'r' : 0x4B, 'g': 0x4B, 'b': 0x4B };
-			} else {
-			if (sender.cells.length > 0 && sender.socket.remoteAddress != config.serverAdminIP)
-				color = sender.cells[0].getColor();
-		}
+	if (sender.socket.remoteAddress == config.serverAdminIP) {
+		color = { 'r' : 0x4B, 'g': 0x4B, 'b': 0x4B };
+	} else {
+		if (sender.cells.length > 0 && sender.socket.remoteAddress != config.serverAdminIP)
+			color = sender.cells[0].getColor();
+	}
     }
 
     var writer = new BinaryWriter();
